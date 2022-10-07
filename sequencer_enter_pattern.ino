a0369e6acc261ceedea2a81617a0e665017f7165
tonylin1918 <115056650+tonylin1918@users.noreply.github.com>
2022-10-07 22:51:07 +1100

    Have LED white 8 Beats  and yellow 16 Beats
    Now need to figure out how to get the beats not mixed up with the first 8 beats when entering 16 beats

--- sequencer_enter_pattern.ino
+++ sequencer_enter_pattern.ino
@@ -6,14 +6,14 @@ int countUp = 0;            //creates a variable integer called 'countUp'
 
 //Button/LED/Motor selection pin
 // initial button values
-int butonPins[16] = {10, 13, 16, 19, 22, 25, 28, 31 ,10, 13, 16, 19, 22, 25, 28, 31};
+int butonPins[16] = {10, 13, 16, 19, 22, 25, 28, 31 , 10, 13, 16, 19, 22, 25, 28, 31};
 int LEDpins[16] = {11, 14, 17, 20, 23, 26, 29, 32, 11, 14, 17, 20, 23, 26, 29, 32};
 bool LEDstates[16] = {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false};
 bool Bstates[16] = {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false};
 bool prevBstates[16] = {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false};
 int motorPins[16] = {12, 15, 18, 21, 24, 27, 30, 33, 12, 15, 18, 21, 24, 27, 30, 33 };
 
- 
+
 unsigned long p = 1000;
 unsigned long sm = 0;  //some global variables available anywhere in the program
 unsigned long cm;
@@ -71,7 +71,7 @@ int blue_light_pin = 37;
 
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
- #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
 #endif
 // Which pin on the Arduino is connected to the NeoPixels?
 #define PIN        8 // On Trinket or Gemma, suggest changing this to 1
@@ -82,7 +82,7 @@ Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 void setup() {
 
   Serial.begin(115200);
-pixels.begin();
+  pixels.begin();
 
   // setup each sequencer button
   for (int i = 0; i < 8; i++) {
@@ -157,8 +157,8 @@ void loop() {
     //menu BPM choices
     //counter if = and led = then
     motorvibrate();
-  Ledstatewheninplaymode();
-}
+    Ledstatewheninplaymode();
+  }
 
 }
 
@@ -184,16 +184,16 @@ void doButtons()
       Serial.println(i);
       LEDstates[i] = !LEDstates[i];
       digitalWrite(LEDpins[i], LEDstates[i]);
-     
+
       if (LEDstates[i] == HIGH) {
         Serial.println("LED is on");
-        
+
       } else {
         Serial.println("LED is off");
       }
     }
-  
-}
+
+  }
 }
 
 
@@ -202,7 +202,7 @@ void menubpmbutton()
 
 {
 
-  
+
   prevMenuBstate = menuBstate;
   menuBstate = digitalRead(menuButtonPin);
   if (prevMenuBstate == HIGH && menuBstate == LOW) {
@@ -275,7 +275,7 @@ void  resetmode()  // use this as creating 8 bar and 16 bars swappping from 1-8 
 {
 
 
-  
+
   prevresetbuttonstate = resetbuttonstate;
   resetbuttonstate = digitalRead(resetbuttonPin);
   if (prevresetbuttonstate == HIGH && resetbuttonstate == LOW) {
@@ -292,65 +292,82 @@ void  resetmode()  // use this as creating 8 bar and 16 bars swappping from 1-8 
 
   if ( counter1 == 0 )  {
     // if only using first page then 8 bars
-   // Serial.println ("8 Bar!");
+    // Serial.println ("8 Bar!");
 
     if (countUp >= 8) {
-      countUp = 0;    }
-        c = 8;
+      countUp = 0;
+    }
+    c = 8;
     // RBG CHANGE COLOUR
-period = 0;
+    period = 0;
 
     for (int i = 0; i < 8; i++) {
-    if (LEDstates[i] == HIGH) {
-    pixels.setPixelColor(i, pixels.Color(150, 150, 150 ));
-    pixels.show();   // Send the updated pixel colors to the hardware.    };
+      if (LEDstates[i] == HIGH) {
+        pixels.setPixelColor(i, pixels.Color(150, 150, 150 ));
+        pixels.show();   // Send the updated pixel colors to the hardware.    };
+      }
+      else {
+        pixels.setPixelColor(i, pixels.Color(0 , 0, 0 ));
+      }
+      pixels.show();   // Send the updated pixel colors to the hardware.    }
     }
-    else { pixels.setPixelColor(i, pixels.Color(0 , 0, 0 ));}
-    pixels.show();   // Send the updated pixel colors to the hardware.    }
-    }}
-     
-
-
-  
-  
+  }
 
 
 
-  
+
+
+
+
+
+
 
 
 
   if ( counter1 == 1 )  {
     // if only using first page then 16 bars
     c = 16;
-    // also double speed 
-//Serial.println ("16 Bar!");
-  // RBG CHANGE COLOUR
+    // also double speed
+    //Serial.println ("16 Bar!");
+    // RBG CHANGE COLOUR
 
-//Can enter into part 2 of 9-16 button 1 = 9
+    //Can enter into part 2 of 9-16 button 1 = 9
 
-  // check for button push on each sequencer button
-  for (int i = 8; i < 16; i++) { 
-    //   Serial.print(Bstates[i]);
-    //  Serial.print(" ");
-    prevBstates[i] = Bstates[i];
-    Bstates[i] = digitalRead(butonPins[i]);
-    if (prevBstates[i] == HIGH && Bstates[i] == LOW) {
-      // the button is pressed
-      Serial.print("Button pressed. Num: ");
-      Serial.println(i);
-      LEDstates[i] = !LEDstates[i];
-      digitalWrite(LEDpins[i], LEDstates[i]);
-      if (LEDstates[i] == HIGH) {
-        Serial.println("LED is on");
-      } else {
-        Serial.println("LED is off");
+    // check for button push on each sequencer button 
+    for (int i = 8; i < 16; i++) {
+
+      prevBstates[i] = Bstates[i];
+      Bstates[i] = digitalRead(butonPins[i]);
+      if (prevBstates[i] == HIGH && Bstates[i] == LOW) {
+        // the button is pressed
+        Serial.print("Button pressed. Num: ");
+        Serial.println(i);
+        LEDstates[i] = !LEDstates[i];
+        digitalWrite(LEDpins[i], LEDstates[i]);
+        if (LEDstates[i] == HIGH) {
+          Serial.println("LED is on");
+        } else {
+          Serial.println("LED is off");
+        }
+
+         {  
+
+          //-8 to get the orignial 0-7 numbers for led
+          if (LEDstates[i] == HIGH) {
+            pixels.setPixelColor(i-8, pixels.Color(150, 150, 0 ));
+            pixels.show();   // Send the updated pixel colors to the hardware.    };
+          }
+          else {
+            pixels.setPixelColor(i-8, pixels.Color(0 , 0, 0 ));
+                    pixels.show();   //(255,255,0) Send the updated pixel colors to the hardware.    }
+}
+        }
       }
-    
+    }
   }
-  }
-  }
-  }
+  
+}
+
 
 
 void timeperperiod()
@@ -376,11 +393,11 @@ void timeperperiod()
       //add counter to loop
 
       countUp++;                //Adds 1 to the countUp int on every loop
-Serial.println (period);
+      Serial.println (period);
 
-  if (countUp >= c) {
-      countUp = 0;
-    }
+      if (countUp >= c) {
+        countUp = 0;
+      }
       Serial.print("-Steps ");
       Serial.println(countUp);
       Serial.print("period: ");
@@ -389,7 +406,7 @@ Serial.println (period);
     else
     { analogWrite(LEDmenu, 0); // Turn the LED on
     }
-  
+
   }
 }
 
@@ -401,13 +418,19 @@ void BPMchoices()
 
 {
 
-  // Double speed for change of 16bars 
+  // Double speed for change of 16bars
   if (c == 16 && period == 500)
-{period = 250;}
+  {
+    period = 250;
+  }
   if (c == 16 && period == 1000)
-{period = 501;}
+  {
+    period = 501;
+  }
   if (c == 16 && period == 750)
-{period = 375;}
+  {
+    period = 375;
+  }
 
 
   for (int i = 0; i < 8; i++) {
@@ -487,28 +510,29 @@ void motorvibrate()
 void  Ledstatewheninplaymode()
 
 { //lights all turn on white fix....
- for (int i = 0; i < 8; i++) {
-       if (countUp == i  && LEDstates[i] == HIGH) {
- pixels.setPixelColor(i, pixels.Color(0, 150, 0 ));
-    pixels.show();   // Send the updated pixel colors to the hardware.
-       }
-       else {
-      
-}
-    
-         if (countUp == !i  && pixels.Color(0, 150, 0 ) && LEDstates[i] == HIGH)  {
-  pixels.show();   // Send the updated pixel colors to the hardware.
-pixels.setPixelColor(i, pixels.Color(150, 150, 150 ));
- pixels.show();   // Send the updated pixel colors to the hardware.
+  for (int i = 0; i < 8; i++) {
+    if (countUp == i  && LEDstates[i] == HIGH) {
+      pixels.setPixelColor(i, pixels.Color(0, 150, 0 ));
+      pixels.show();   // Send the updated pixel colors to the hardware.
+    }
+    else {
+
+    }
+
+    if (countUp == !i  && pixels.Color(0, 150, 0) && LEDstates[i] == HIGH)  {
+      pixels.show();   // Send the updated pixel colors to the hardware.
+      pixels.setPixelColor(i, pixels.Color(150, 150, 150 ));
+      pixels.show();   // Send the updated pixel colors to the hardware.
+
+    }
 
   }
-  
-}}
+}
 
 
 
 
- 
+
 void RGB_color(int red_light_value, int green_light_value, int blue_light_value)
 {
   analogWrite(red_light_pin, red_light_value);
